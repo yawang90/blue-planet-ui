7b32dfd32db9ce3348f89dac31b80e84bfff67cf
yawang <yan.wang@gmx.at>
2023-11-24 11:59:28 +0100

    Add initial setup of FE Angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatGridListModule} from "@angular/material/grid-list";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatGridListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'blue-planet';
+  title = 'Climate school report';
 }
