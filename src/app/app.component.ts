30cf6fb98bb508d40fb4cd8967fe10b04d04223c
yawang <yan.wang@gmx.at>
2023-11-24 14:50:39 +0100

    Add biodiversity and energie components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import {Router, RouterOutlet} from '@angular/router';
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatIconModule} from "@angular/material/icon";
+import {BiodiversityComponent} from "../biodiversity/biodiversity.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatGridListModule, MatIconModule],
+  imports: [CommonModule, RouterOutlet, MatGridListModule, MatIconModule, BiodiversityComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
